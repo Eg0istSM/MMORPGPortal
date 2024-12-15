c8d76ce79d2a3b3dcfddc94186124762a6e73197
Eg0istSM <Frolov27101993@yandex.ru>
2024-12-15 16:14:32 +0300

    first commit

---
+++ MMORPG/portal/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Users(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class CategoryRole(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    subscribers = models.ManyToManyField(User, blank=True, null=True, related_name='categories')
+
+
+class Announcement(models.Model):
+    author = models.ForeignKey(Users, on_delete=models.CASCADE)
+    text = models.TextField()
+    title = models.CharField(max_length=250)
+    image = models.ImageField()
+    video = models.FileField()
+    category = models.ManyToManyField(CategoryRole, through='PostCategoryRole')
+    time_public = models.DateTimeField(auto_now_add=True)
+
+
+class Response(models.Model):
+    announcement = models.ForeignKey(Announcement, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    time_response = models.DateTimeField(auto_now_add=True)
+
+
+
+
