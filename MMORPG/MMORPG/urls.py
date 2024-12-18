95253be495fac940cae44a863128c8f1016b5351
Eg0istSM <Frolov27101993@yandex.ru>
2024-12-18 21:11:15 +0300

    broke urls

--- MMORPG/MMORPG/urls.py
+++ MMORPG/MMORPG/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('portal/', include('portal.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
--- MMORPG/portal/urls.py
+++ MMORPG/portal/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
 from .views import *
-from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
     path('confirm/', ConfirmUser.as_view(), name='confirm_user'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('announcement/', AnnouncementsList.as_view(), name='announcements'),
-    path('login/', LoginView.as_view(template_name='account/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='account/logout.html'), name='logout'),
-
-
 
 ]
