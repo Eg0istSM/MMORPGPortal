1b08431d6bcfd0d1bc2f75e7933fefea3710dd05
Eg0istSM <Frolov27101993@yandex.ru>
2024-12-18 21:22:29 +0300

    broke urls

--- MMORPG/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('portal/', include('portal.urls')),
-    path('accounts/', include('allauth.urls')),
-]
